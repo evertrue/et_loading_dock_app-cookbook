685e94f7d2fb92b42a3093af9f8398e24884896d
Jeff Byrnes <jeff@evertrue.com>
2014-06-23 16:31:51 -0400

    Add missing command stub to ChefSpec tests

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -41,6 +41,8 @@ describe 'et_loading_dock_app::default' do
         'secret_access_key' => 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
       }
     )
+
+    stub_command('which nginx').and_return('/usr/sbin/nginx')
   end
 
   it 'includes et_rails_app' do
