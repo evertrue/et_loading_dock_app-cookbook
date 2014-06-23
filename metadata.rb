ad13ca0626c8ce6709a166bd7ea2805514366d53
Jeff Byrnes <jeff@evertrue.com>
2014-06-23 16:17:42 -0400

    Bump to et_rails_app ~> 5.1.4

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures et_loading_dock_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
 
-depends 'et_rails_app', '~> 3.2.0'
+depends 'et_rails_app', '~> 5.1.4'
 depends 'apt'
