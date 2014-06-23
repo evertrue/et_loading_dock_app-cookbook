6cfabf11bd1631a2017b1327ebf75b4fa96444b0
Jeff Byrnes <jeff@evertrue.com>
2014-06-23 16:46:17 -0400

    Version bump to 2.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_loading_dock_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '2.0.0'
 
 depends 'et_rails_app', '~> 5.1.4'
 depends 'apt'
