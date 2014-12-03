0fe47a1dc91dba8d58b8dc312a3c2bd144de55c3
Jeff Byrnes <jeff@evertrue.com>
2014-12-03 17:10:15 -0500

    Use new ChefSpec syntax for Runners

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'et_loading_dock_app::default' do
   let(:chef_run) do
-    ChefSpec::Runner.new do |node|
+    ChefSpec::SoloRunner.new do |node|
       # Create a new environment
       env = Chef::Environment.new
       env.name 'prod'
