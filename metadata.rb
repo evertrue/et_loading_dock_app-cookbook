a44fc82722a7fa0477f68c9ab3aa9dc5d968501a
Jeff Byrnes <jeff@evertrue.com>
2014-12-03 17:10:59 -0500

    Looser pin to newer version of et_rails_app

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures et_loading_dock_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
 
-depends 'et_rails_app', '~> 5.1.4'
+depends 'et_rails_app', '~> 5.2'
 depends 'apt'
